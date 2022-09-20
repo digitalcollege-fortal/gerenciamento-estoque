c0831e6372b021c24af3a77cc8b32313705f0961
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2022-09-19 21:58:13 -0300

    Listar e remover categorias

--- js/pages/categories.js
+++ js/pages/categories.js
@@ -1,18 +1,29 @@
+function removeCategory (id) {
+    fetch ('http://localhost:8000/categorias/'+id, {
+        method: 'DELETE',
+    });
+}
+
 function categories() {
-    let data = [1, 2, 3, 4, 5].map( (item) => {
-        return `
-            <tr>
-                <td>${item}</td>
-                <td>Informática</td>
-                <td>Descrição</td>
-                <td>- Foto -</td>
-                <td>
-                    <button>Editar</button>
-                    <button>Excluir</button>
-                </td>
-            </tr>
-        `;
-    } );
+    fetch('http://localhost:8000/categorias')
+        .then(response => response.json())
+        .then(dados => {
+            dados.map(cat => {
+                table_categories.innerHTML += `
+                    <tr>
+                        <td>${cat.id}</td>
+                        <td>${cat.nome}</td>
+                        <td>${cat.descricao}</td>
+                        <td><img width="100px" src="${cat.foto}"></td>
+                        <td>
+                            <button class="btn btn-outline-warning">Editar</button>
+                            <button onclick="removeCategory(${cat.id})" class="btn btn-outline-danger">Excluir</button>
+                        </td>
+                    </tr>
+                `;
+            });
+        });
+
 
     return `
         <h1>Listar Categorias</h1>
@@ -28,8 +39,8 @@ function categories() {
                     <th>Ações</th>
                 </tr>
             </thead>
-            <tbody>
-                ${data.join('')}
+            <tbody id="table_categories">
+                
             </tbody>
         </table>
     `;
